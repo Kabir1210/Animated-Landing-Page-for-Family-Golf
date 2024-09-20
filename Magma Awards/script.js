620da7a2bbbe75064cd6f986ee4ded21e3cd4b83
Atharva P. Talaghatti <atharva.talaghatti20@pccoepune.org>
2024-09-20 15:30:49 +0530

    20/9/24
    New Updates

--- Magma Awards/script.js
+++ Magma Awards/script.js
@@ -618,3 +618,27 @@ end: `250% top`,
 });
 }
 canvas2()
+
+gsap.to("#page7 .page7-cir",{
+  scrollTrigger:{
+    trigger:'.page7-cir',
+    start:'top center',
+    end:'bottom top',
+    markers:true,
+    scroller:'#main',
+    scrub:.5
+  },
+  scale:1.5
+})
+
+gsap.to("#page7 .page7-cir-inner",{
+  scrollTrigger:{
+    trigger:'.page7-cir-inner',
+    start:'top center',
+    end:'bottom top',
+    markers:true,
+    scroller:'#main',
+    scrub:.5
+  },
+  backgroundColor: `#0b48ed`
+})
